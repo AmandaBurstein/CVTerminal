3a71e886516cecf3f24899c1826fd9cfdd55386f
AmandaBurstein <amburst@gmail.com>
2020-01-23 17:06:20 -0500

    Added <Pricing /> to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./Pages/HomePage/HomePage.jsx";
 import Features from "./Pages/HomePage/Components/Feature/Features";
+import Pricing from "./Pages/HomePage/Components/Pricing/Pricing.jsx";
 
 function App() {
   return (
     <React.Fragment>
       <HomePage />
       <Features />
+      <Pricing />
     </React.Fragment>
   );
 }
